6b7aef2e1d0e3879bfd58f1db3f63c0408d1c63f
SameerBatla <sambatla456@gmail.com>
2020-02-17 03:04:21 +0530

    broadcast message and giving final touch

--- public/chat.js
+++ public/chat.js
@@ -4,6 +4,7 @@ const message = document.getElementById('message');
 const handle = document.getElementById('handle');
 const btn = document.getElementById('send');
 const output = document.getElementById('output');
+const feedback = document.getElementById('feedback');
 
 //events
 
@@ -14,7 +15,16 @@ btn.addEventListener('click', () => {
   });
 });
 
+message.addEventListener('keypress', () => {
+  socket.emit('typing', handle.value);
+});
+
 socket.on('chat', data => {
+  feedback.innerHTML = '';
   output.innerHTML +=
     '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+});
+
+socket.on('typing', data => {
+  feedback.innerHTML = '<p><em>' + data + ' is typing....' + '</em></p>';
 });
