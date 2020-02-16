eac4466fbdb0bcef141edb41334887d3fb83d34e
SameerBatla <sambatla456@gmail.com>
2020-02-17 02:11:17 +0530

    made socket connection using socket.io

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
-app.listen(4000, () => {
+
+const server = app.listen(4000, () => {
   console.log('listining on port');
 });
 
 app.use(express.static('public'));
+
+const io = socket(server);
+io.on('connection', socket => {
+  console.log('made the connection');
+});
